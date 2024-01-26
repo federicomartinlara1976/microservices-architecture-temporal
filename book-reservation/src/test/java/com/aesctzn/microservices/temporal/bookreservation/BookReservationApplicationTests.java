e5ce73290ea7cdfba6db74c45ee98c828107435f
Antonio Escobar <aescobartizon@gmail.com>
2024-01-26 16:12:38 +0100

    Init proyect

---
+++ book-reservation/src/test/java/com/aesctzn/microservices/temporal/bookreservation/BookReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aesctzn.microservices.temporal.bookreservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ book-stock/src/main/java/com/aesctzn/microservices/temporal/bookreservation/BookReservationApplication.java
@@ -0,0 +1,13 @@
+package com.aesctzn.microservices.temporal.bookreservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookReservationApplication.class, args);
+	}
+
+}
