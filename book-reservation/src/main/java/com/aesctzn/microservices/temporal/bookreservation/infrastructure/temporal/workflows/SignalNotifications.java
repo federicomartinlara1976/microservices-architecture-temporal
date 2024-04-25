57ccb6b6916af8a385da2441b93c58f851742d2a
federicomartinlara1976@gmail.com <federicomartinlara1976@gmail.com>
2024-04-25 22:22:09 +0200

    Anotaciones

--- book-reservation/src/main/java/com/aesctzn/microservices/temporal/bookreservation/infrastructure/temporal/workflows/SignalNotifications.java
+++ book-reservation/src/main/java/com/aesctzn/microservices/temporal/bookreservation/infrastructure/temporal/workflows/SignalNotifications.java
@@ -1,17 +1,15 @@
 package com.aesctzn.microservices.temporal.bookreservation.infrastructure.temporal.workflows;
 
-import com.aesctzn.microservices.temporal.bookreservation.domain.Book;
 import com.aesctzn.microservices.temporal.bookreservation.domain.Reservation;
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class SignalNotifications {
-
-    public SignalNotifications(){
-
-    }
 
     private boolean sendNotification = false;
 
